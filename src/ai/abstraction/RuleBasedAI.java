859aff0b73064bbd61f48209ad235951c6a86ece
ASH <alexhollander522@gmail.com>
2016-05-17 20:27:55 -0400

    Added requirements for rules based system.

---
+++ src/ai/abstraction/RuleBasedAI.java
@@ -0,0 +1,44 @@
+/*
+ * [x] = completed
+ *
+ * Requirements Checklist
+ *  Rule Based System
+ *       1. Knowledge Base
+ *       2. Inference Engine 
+ *           a. Implements a form of unification 
+ *               i. Optional: variables
+ *       3. Rule Base
+ *           a. Cannot be hardcoded if-then-logic
+ *           b. Classes for how to store rules
+ *           c.  
+ */
+package ai.abstraction;
+
+import ai.abstraction.pathfinding.PathFinding;
+import ai.core.AI;
+import rts.GameState;
+import rts.PlayerAction;
+
+
+public class RuleBasedAI extends AbstractionLayerAI {
+
+    public RuleBasedAI(PathFinding a_pf) {
+        super(a_pf);
+    }
+
+    @Override
+    public void reset() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public AI clone() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public PlayerAction getAction(int player, GameState gs) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
